f2669e8c608e580ceb6f9042ee1733f4d56ffc5a
KanDai <KanDai@users.noreply.github.com>
2023-11-24 09:57:02 +0900

    update script

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-Google Apps Script development environment using [clasp](https://github.com/google/clasp#create)
-
-## Hou to use
-
-```
-npm install
-```
-
-```
-npm run init -title="YOUR_SCRIPT_NAME"
-```
+A列に記載したURLのtitleとdescriptionを取得してスプレッドシートに記載するサンプル
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,39 @@
-const hello = () => {
-  console.log('Hello World!')
+const getHtmlString = (url: string) => {
+  return UrlFetchApp.fetch(url).getContentText('UTF-8')
+}
+
+const getTitle = (htmlString: string) => {
+  const matches = htmlString.match(/<title>([^<]+)<\/title>/)
+
+  return matches && matches.length > 1 ? matches[1] : ''
+}
+
+const getDescription = (htmlString: string) => {
+  const matches = htmlString.match(
+    /<meta name="description" content="([^"]+)">/
+  )
+
+  return matches && matches.length > 1 ? matches[1] : ''
+}
+
+const getUrls = () => {
+  const sheet = SpreadsheetApp.getActiveSheet()
+  const lastRow = sheet.getLastRow()
+  const urls = sheet.getRange(2, 1, lastRow - 1).getValues()
+  return urls.flat()
+}
+
+const updateSheet = (urls: string[]) => {
+  const sheet = SpreadsheetApp.getActiveSheet()
+  urls.forEach((url, index) => {
+    const htmlString = getHtmlString(url)
+    const title = getTitle(htmlString)
+    const description = getDescription(htmlString)
+    sheet.getRange(index + 2, 2, 1, 2).setValues([[title, description]])
+  })
+}
+
+const main = () => {
+  const urls = getUrls()
+  updateSheet(urls)
 }
